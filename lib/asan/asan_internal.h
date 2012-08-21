5b429f53b2567081cb8c83ec3e03258ab6fb9fae
Dan McGregor <dan.mcgregor@usask.ca>
2012-08-21 08:30:33 -0600

    Conform to LLVM style manual.

--- lib/asan/asan_internal.h
+++ lib/asan/asan_internal.h
@@ -19,7 +19,8 @@
 #include "sanitizer_common/sanitizer_internal_defs.h"
 #include "sanitizer_common/sanitizer_libc.h"
 
-#if !defined(__linux__) && !defined(__APPLE__) && !defined(_WIN32) && !defined(__FreeBSD__)
+#if !defined(__linux__) && !defined(__APPLE__) && !defined(_WIN32) && \
+    !defined(__FreeBSD__)
 # error "This operating system is not supported by AddressSanitizer"
 #endif
 
